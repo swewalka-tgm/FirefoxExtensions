823178f894229d9c57e63c8188b6f9b4b5b2f972
swewalka-tgm <swewalka@student.tgm.ac.at>
2023-04-14 16:36:18 +0200

    first extension made

---
+++ imageChanger/src/main.js
@@ -0,0 +1,7 @@
+console.log("The extension is up and running");
+
+var images = document.getElementsByTagName("img");
+
+// for (elem of images){
+//     elem.src = `${browser.runtime.getURL("../img/replacement.jpg")}`
+// }
